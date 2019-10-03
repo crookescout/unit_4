079d530383f5f9ff3cc5924fd60c56b2dbae8cad
scout <scout.crooke@ssfs.org>
2019-10-03 11:01:19 -0400

    Initial commit

---
+++ daily.py
@@ -0,0 +1,9 @@
+# by Scout Crooke, 10/3/19, this program does daily exercises
+
+num = float(input("Wat is the number?"))
+
+if num % 2 = 0
+    print("This is an even number")
+
+if num % 2 = 1
+    print("This is an odd number")
