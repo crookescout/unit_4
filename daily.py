62ca145cd7dd476457b1ef5a33569d38910934e2
scout <scout.crooke@ssfs.org>
2019-10-03 11:21:07 -0400

    Worked on daily exercises

--- daily.py
+++ daily.py
@@ -1,9 +1,26 @@
 # by Scout Crooke, 10/3/19, this program does daily exercises
 
-num = float(input("Wat is the number?"))
+num = float(input("What is the number?"))
 
-if num % 2 = 0
+if num % 2 == 0:
     print("This is an even number")
 
-if num % 2 = 1
+else:
     print("This is an odd number")
+
+
+def is_divisible(num, check):
+    if num % check == 0:
+        return True
+    else:
+        return False
+
+
+def main():
+    num = float(input("What is the first number?"))
+    check = float(input("What is the second number?"))
+    if is_divisible(num, check):
+        print("Yes,", num, "is divisible by", check)
+
+
+main()
