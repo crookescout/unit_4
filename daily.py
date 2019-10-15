b1e7ca248b390d8cd741eda798025752fb0c80d4
scout <scout.crooke@ssfs.org>
2019-10-15 10:16:06 -0400

    Finished daily unit 4 daily exercises

--- daily.py
+++ daily.py
@@ -2,13 +2,12 @@
 
 import random
 
-num = float(input("What is the number?"))
-
-if num % 2 == 0:
-    print("This is an even number")
-
-else:
-    print("This is an odd number")
+# num = float(input("What is the number?"))
+#
+# if num % 2 == 0:
+#     print("This is an even number")
+# else:
+#     print("This is an odd number")
 
 
 def is_divisible(num, check):
@@ -37,7 +36,7 @@ def rock_paper_scissors():
     user_choice = input("Let's play rock-paper-scissors! Choose one!")
     computer_choice = computer()
     if computer_choice == 1:
-        print("Computer picked rock")
+        print("Computer chose rock")
         if user_choice == "rock":
             print("Tie!")
         elif user_choice == "scissors":
@@ -45,7 +44,7 @@ def rock_paper_scissors():
         else:
             print("Sorry, you lose!")
     if computer_choice == 2:
-        print("Computer picked paper")
+        print("Computer chose paper")
         if user_choice == "paper":
             print("Tie!")
         elif user_choice == "rock":
@@ -53,7 +52,7 @@ def rock_paper_scissors():
         else:
             print("Sorry, you lose!")
     if computer_choice == 3:
-        print("Computer picked scissors")
+        print("Computer chose scissors")
         if user_choice == "scissors":
             print("Tie!")
         elif user_choice == "paper":
@@ -62,24 +61,39 @@ def rock_paper_scissors():
             print("Sorry, you lose!")
 
 
+def caught_speeding(speed, birthday):
+    if birthday:
+        speed -= 5
+    if speed <= 60:
+        return 0
+    elif speed >= 61 and speed <= 80:
+        return 1
+    else:
+        return 2
+
+
+
 def main():
-    num = float(input("What is the first number?"))
-    check = float(input("What is the second number?"))
-    if is_divisible(num, check):
-        print("Yes,", num, "is divisible by", check)
-    else:
-        print("No,", num, "is not divisible by", check)
+    # num = float(input("What is the first number?"))
+    # check = float(input("What is the second number?"))
+    # if is_divisible(num, check):
+    #     print("Yes,", num, "is divisible by", check)
+    # else:
+    #     print("No,", num, "is not divisible by", check)
+    #
+    # s1 = float(input("What is the length of side #1?"))
+    # s2 = float(input("What is the length of side #2?"))
+    # s3 = float(input("What is the length of side #3?"))
+    # if is_triangle(s1, s2, s3):
+    #     print("Yes")
+    # else:
+    #     print("No")
+    #
+    # computer()
+    # rock_paper_scissors()
 
-    s1 = float(input("What is the length of side #1?"))
-    s2 = float(input("What is the length of side #2?"))
-    s3 = float(input("What is the length of side #3?"))
-    if is_triangle(s1, s2, s3):
-        print("Yes")
-    else:
-        print("No")
+    print(caught_speeding(65, True))
 
-    computer()
-    rock_paper_scissors()
 
 main()
 
