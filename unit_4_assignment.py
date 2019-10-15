c92bd728f55b9a953bc19dfc0388a4f0ed9558b9
scout <scout.crooke@ssfs.org>
2019-10-15 09:59:04 -0400

    Finished unit 4 assignment option 2

--- unit_4_assignment.py
+++ unit_4_assignment.py
@@ -4,21 +4,58 @@ import random
 
 
 def get_max_num():
+    """
+    This function gets the user's maximum number which could be a decimal
+    :return: maximum number as a float
+    """
     max_num = float(input("What is the maximum you would like?"))
     return max_num
 
 
 def get_problem_type():
-    problem_type = float(input("What type of problem would you like?"))
+    """
+    This function gets the user's type of math problem and if the user does not type one of the specific types of
+    problems, it will automatically become an addition problem
+    :return: type of problem as a string
+    """
+    problem_type = str(input("What type of math problem would you like? (*, -, or +)"))
     return problem_type
+
+
+def compare_answers(user_answer, correct_answer):
+    """
+    This function compares the user's answer with the correct answer and tells the user if they got the problem correct
+    :param user_answer:
+    :param correct_answer:
+    :return: none
+    """
+    if user_answer == correct_answer:
+        print("Great work! You got the correct answer!")
+    else:
+        print("Sorry, that is not the correct answer")
+
 
 def main():
     maximum = get_max_num()
     problem = get_problem_type()
-    random.randint(0, maximum)
-    if problem == "+":
-        print("User picked addition")
+    random1 = random.randint(0, maximum)
+    random2 = random.randint(0, maximum)
+    if problem == "*":
+        print("User picked multiplication. Here is the problem:")
+        print(random1, "*", random2)
+        user_answer = float(input("What is the answer to this problem?"))
+        compare_answers(user_answer, random1 * random2)
+    elif problem == "-":
+        print("User picked subtraction. Here is the problem:")
+        print(random1, "-", random2)
+        user_answer = float(input("What is the answer to this problem?"))
+        compare_answers(user_answer, random1 - random2)
+    else:
+        print("User picked addition. Here is the problem:")
+        print(random1, "+", random2)
+        user_answer = float(input("What is the answer to this problem?"))
+        compare_answers(user_answer, random1 + random2)
+    print("Thanks for playing!")
 
 
 main()
-
